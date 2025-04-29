a775b3af296bed8a0208f474e7b909de6e9a7da9
Philipp_Roeder <p.roeder@xceptance.net>
2025-04-29 13:51:58 +0200

    added a component for progress indicator in checkout

---
+++ src/main/java/com/xceptance/loadtest/posters/model/components/checkout/ProgressIndicator.java
@@ -0,0 +1,27 @@
+package com.xceptance.loadtest.posters.model.components.checkout;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import com.xceptance.loadtest.api.components.SelenideComponent;
+
+import static com.codeborne.selenide.Selenide.$;
+
+/**
+ * Progress Indicator Bar component in Checkout.
+ *
+ * @author Xceptance Software Technologies
+ */
+public class ProgressIndicator implements SelenideComponent
+{
+    @Override
+    public SelenideElement locate()
+    {
+        return $("#checkout-progress-indicator");
+    }
+
+    @Override
+    public void validate()
+    {
+        locate().should(Condition.exist);
+    }
+}
