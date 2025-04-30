b0bf7a58c3107142444f284bcc53a5c9464bcdc0
Philipp_Roeder <p.roeder@xceptance.net>
2025-04-30 09:17:53 +0200

    Moved locale switcher from general page to a new class

---
+++ src/main/java/com/xceptance/loadtest/posters/model/components/general/LocaleSwitcher.java
@@ -0,0 +1,53 @@
+package com.xceptance.loadtest.posters.model.components.general;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import com.xceptance.loadtest.api.components.SelenideComponent;
+import com.xceptance.loadtest.api.util.Action;
+
+import static com.codeborne.selenide.Selenide.$;
+
+/**
+ * Choose another locale
+ *
+ * @author Xceptance Software Technologies
+ */
+public class LocaleSwitcher implements SelenideComponent
+{
+    @Override
+    public SelenideElement locate()
+    {
+        return $(".nav-item.dropdown > #show-locale-menu");
+    }
+
+    @Override
+    public void validate()
+    {
+        locate().should(Condition.exist);
+    }
+
+    public void switchToUs()
+    {
+        Action.run("SwitchToUS", () ->
+        {
+            locate().hover();
+            $("#locale-menu [href*='US']").click();
+
+            // verify active locale
+            locate().should(Condition.exactText("en-US"));
+        });
+    }
+
+    public void switchToDe()
+    {
+        Action.run("SwitchToDE", () ->
+        {
+
+            locate().hover();
+            $("#locale-menu [href*='DE']").click();
+
+            // verify active locale
+            locate().should(Condition.exactText("de-DE"));
+        });
+    }
+}
--- src/main/java/com/xceptance/loadtest/posters/model/pages/GeneralPage.java
+++ src/main/java/com/xceptance/loadtest/posters/model/pages/GeneralPage.java
@@ -6,6 +6,7 @@ import com.codeborne.selenide.Condition;
 import com.xceptance.loadtest.api.pages.PageInterface;
 import com.xceptance.loadtest.api.util.Action;
 import com.xceptance.loadtest.posters.model.components.general.Footer;
+import com.xceptance.loadtest.posters.model.components.general.LocaleSwitcher;
 import com.xceptance.loadtest.posters.model.components.general.MiniCart;
 import com.xceptance.loadtest.posters.model.components.general.Navigation;
 import com.xceptance.loadtest.posters.model.components.general.User;
@@ -23,6 +24,8 @@ public class GeneralPage implements PageInterface
 
     public Navigation navigation = new Navigation();
 
+    public LocaleSwitcher localeSwitcher = new LocaleSwitcher();
+
     public Footer footer = new Footer();
 
     @Override
@@ -32,12 +35,13 @@ public class GeneralPage implements PageInterface
         user.validate();
         navigation.validate();
         footer.validate();
+        localeSwitcher.validate();
     }
 
     @Override
     public boolean is()
     {
-        return miniCart.exists() && user.exists() && navigation.exists() && footer.exists();
+        return miniCart.exists() && user.exists() && navigation.exists() && localeSwitcher.exists() && footer.exists();
     }
 
 	public static void search(final String phrase, final String expectedCount)
@@ -54,29 +58,4 @@ public class GeneralPage implements PageInterface
 			$("#total-product-count").should(Condition.exactText(expectedCount));
 		});
 	}
-
-    public static void switchToUs()
-    {
-        Action.run("SwitchToUS", () ->
-        {
-            $("#show-locale-menu").hover();
-            $("#locale-menu [href*='US']").click();
-
-            // verify count
-            $("#show-locale-menu").should(Condition.exactText("en-US"));
-        });
-    }
-
-    public static void switchToDe()
-    {
-        Action.run("SwitchToDE", () ->
-        {
-
-            $("#show-locale-menu").hover();
-            $("#locale-menu [href*='DE']").click();
-
-            // verify count
-            $("#show-locale-menu").should(Condition.exactText("de-DE"));
-        });
-    }
 }
